77dbc5318f3b90f3b94e5e746f49ab968b1eeaf4
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-07-17 18:00:54 -0500

    starting to build routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Container>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
